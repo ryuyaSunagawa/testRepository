766c7e18c2900f31b4d736e06177cbf22aea6657
ryuyaSunagawa <11806134@stu.kbc.ac.jp>
2020-03-19 18:39:02 +0900

    make HelloWorld and Calculation Class

--- testprogram.cpp
+++ testprogram.cpp
@@ -2,12 +2,81 @@
 
 using namespace std;
 
+/*******************************/
+/*****   HelloWorldClass   *****/
+/*******************************/
+class HelloWorld {
+    public :
+
+        HelloWorld();
+
+        void GetValue( int value ) {
+            helloValue = value;
+        }
+
+        int ReturnValue() {
+            return helloValue;
+        }
+
+    private :
+        int helloValue;
+}
+
+HelloWorld :: HelloWorld() {
+    helloValue = 0;
+}
+
+/*********************************/
+/*****    CalculationClass   *****/
+/*********************************/
+class Calculation {
+    public :
+
+        Calculation( int right, int left );
+        Calculation( double right, double left );
+
+        int Plus( int right, int left ) {
+            return calRight + calLeft;
+        }
+
+        int Minus() {
+            return calRight - calLeft;
+        }
+
+    private :
+        int calRighti, calLefti;
+        double calRightd, calLeftd;
+}
+
+Calculation :: Calculation( int right, int left ) {
+    calRighti = right;
+    calLefti = left;
+
+    calRightd = 0;
+    calLeftd = 0;
+}
+
+Calculation :: Calculation( doble right, double left ) {
+    calRighti = 0;
+    calLefti = 0;
+
+    calRightd = right;
+    calLeftd = left;
+}
+
 int main() {
+
+    HelloWorld hello;
+
+    hello.GetValue( 2 );
+
     cout << "HelloGif" << endl;
 
     cout << "HunkTry" << endl;
 
     cout << "HunkTry2" << endl;
+
+    cout << hello.ReturnValue() << endl;
 
     return 0;
 }
